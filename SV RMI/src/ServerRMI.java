f00c9e5add05e1db27583183444ddd6548372093
Anthony Rodriguez <anthony81199@gmail.com>
2020-12-03 00:31:56 -0400

    se agrega case a servidor A

--- SV RMI/src/ServerRMI.java
+++ SV RMI/src/ServerRMI.java
@@ -9,6 +9,7 @@
  * @author Alexis
  */
 
+import java.io.File;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -16,6 +17,12 @@ import java.rmi.server.UnicastRemoteObject;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class ServerRMI extends UnicastRemoteObject implements Middleware {
     
@@ -25,7 +32,40 @@ public class ServerRMI extends UnicastRemoteObject implements Middleware {
     
     @Override
     public String[] pedirLibro(String valor) throws RemoteException {
-        String[] libro = new String[1];
+        String[] libro = new String[4];
+        try {
+            File file = new File("src\\DB\\books.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();  
+            Document doc = db.parse(file);
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName()); 
+            
+            NodeList nodeList = doc.getElementsByTagName("libro");
+            
+            for (int itr = 0; itr < nodeList.getLength(); itr++){  
+                Node node = nodeList.item(itr);  
+                System.out.println("\nNode Name :" + node.getNodeName());  
+                if (node.getNodeType() == Node.ELEMENT_NODE){  
+                    Element eElement = (Element) node;
+                    if (eElement.getElementsByTagName("titulo").item(0).getTextContent().matches(valor)) {
+                        libro[0] = eElement.getElementsByTagName("titulo").item(0).getTextContent();
+                        libro[1] = eElement.getElementsByTagName("autor").item(0).getTextContent();
+                        libro[2] = eElement.getElementsByTagName("editorial").item(0).getTextContent();
+                        libro[3] = eElement.getElementsByTagName("fecha").item(0).getTextContent();
+                        System.out.println("Titulo: "+ eElement.getElementsByTagName("titulo").item(0).getTextContent());  
+                        System.out.println("Autor: "+ eElement.getElementsByTagName("autor").item(0).getTextContent());  
+                        System.out.println("Editorial: "+ eElement.getElementsByTagName("editorial").item(0).getTextContent());  
+                        System.out.println("Fecha: "+ eElement.getElementsByTagName("fecha").item(0).getTextContent()); 
+                    } 
+                }
+            }
+        }
+        catch (Exception e){  
+            e.printStackTrace();
+        }
+        
         return libro;
     }
     
@@ -36,7 +76,9 @@ public class ServerRMI extends UnicastRemoteObject implements Middleware {
     
     @Override
     public String[] getTitle(String value) throws RemoteException {
-        return this.pedirLibro(value);
+        String[] libro;
+        libro = this.pedirLibro(value);
+        return libro;
     }
     
     @Override
