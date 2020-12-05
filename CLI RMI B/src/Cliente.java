a52f421265f60301f6882801160cb286ec5ae1d0
Maxikk26 <maximiliano.bogo@gmail.com>
2020-12-05 17:54:42 -0400

    MOD eliminacion de threads en cliente

--- CLI RMI B/src/Cliente.java
+++ CLI RMI B/src/Cliente.java
@@ -10,6 +10,7 @@
  */
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
 
 public class Cliente {
     public static void main(String[] args) {
@@ -18,14 +19,95 @@ public class Cliente {
     }
     
     private void connectServer() {
-        try { 
-            /* Peticion request = new Peticion("B","100 años de soledad","");
-            Peticion request2 = new Peticion("B","Luna lunera",""); */
-            Peticion request = new Peticion("B","","Antonio Banderas");
-            request.start();
-            //request2.start();
+        try {
+            ArrayList<String> libro = new ArrayList();
+            ArrayList<ArrayList> autorLibros = new ArrayList();
+            String biblioteca = "B";
             
-            
+            // SI ES de A --> A, no pasa por el middleware
+            switch (biblioteca) {
+                case "A":
+                    {
+                        // PEDIR LIBRO XX
+                        Registry registro = LocateRegistry.getRegistry("192.168.99.1", 7777); //pedir al servidor A
+                        Middleware interfaz = (Middleware)registro.lookup("RemoteRMI");
+                        // llama a pedir libro en servidor A
+                        // SI NO HAY EN SERVIDOR A, entonces va de A-->B por lo tanto pasa por el middleware
+                        // Transformar a Z39
+                        // Enviar A servidor B
+                        
+                        // ------------------------------------------------------------------
+                        /* libro = interfaz.pedirLibro("100 años de soledad");
+                        autorLibros = interfaz.pedirAutor("Antonio Banderas","A"); */
+                        
+                       /*  if (libro == null){
+                            System.out.println("Libro no encontrado");
+                        }
+                        else {
+                            libro.forEach((item) -> System.out.println(item));
+                            //System.out.println(autor);
+                        }
+                        
+                        for(int i=0 ; i < autorLibros.size() ; i++) {
+                            for(int j=0 ; j < autorLibros.get(i).size() ; j++) {
+                                System.out.println(autorLibros.get(i).get(j));
+                            } 
+                        }  */
+                    }
+                case "B":
+                    {
+                        Registry registro = LocateRegistry.getRegistry("192.168.56.1", 7778); //pedir al servidor B
+                        Middleware interfaz = (Middleware)registro.lookup("RemoteRMIB");
+                        // llama a pedir libro en servidor B
+                        libro = interfaz.getTitle("100 años de soledad","");
+                        //autorLibros = interfaz.getAuthor("Antonio Banderas","");
+                        
+                        //System.out.println("La suma es " + suma);
+                        if (libro == null){
+                            System.out.println("Libro no encontrado");
+                        }
+                        else {
+                             libro.forEach((item) -> System.out.println(item));
+                            //System.out.println(autor);
+                        }
+                        
+                        for(int i=0 ; i < autorLibros.size() ; i++) {
+                            for(int j=0 ; j < autorLibros.get(i).size() ; j++) {
+                                System.out.println(autorLibros.get(i).get(j));
+                            } 
+                        }
+                        
+                        for(ArrayList<ArrayList> items:autorLibros) {
+                            for(ArrayList<String> item: items) {
+                                for(String i: item){
+                                    System.out.println(i);
+                                }
+                            }  
+                        }
+                    }
+                case "C":
+                    {
+                        Registry registro = LocateRegistry.getRegistry("192.168.56.1", 7778); //pedir al servidor c
+                        Middleware interfaz = (Middleware)registro.lookup("RemoteRMIC");
+                        // llama a pedir libro en servidor c
+                        libro = interfaz.getTitle("100 años de soledad","C");
+                        autorLibros = interfaz.getAuthor("Antonio Banderas","C");
+                        
+                        if (libro == null){
+                            System.out.println("Libro no encontrado");
+                        }
+                        else {
+                             libro.forEach((item) -> System.out.println(item));
+                            //System.out.println(autor);
+                        }
+                        
+                        for(int i=0 ; i < autorLibros.size() ; i++) {
+                            for(int j=0 ; j < autorLibros.get(i).size() ; j++) {
+                                System.out.println(autorLibros.get(i).get(j));
+                            } 
+                        } 
+                    }
+            }
         } catch (Exception ex){
             System.out.println(ex);
         }
