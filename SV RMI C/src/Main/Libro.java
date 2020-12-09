b68c214acd4976a4b9e0802f7315c6e40502e72a
alexjose131 <ajfernandez.17@est.ucab.edu.ve>
2020-12-09 01:47:36 -0400

    se agrega servidor C

---
+++ SV RMI C/src/Main/Libro.java
@@ -0,0 +1,83 @@
+package Main;
+
+import javax.xml.parsers.DocumentBuilderFactory;  
+import javax.xml.parsers.DocumentBuilder;  
+import org.w3c.dom.Document;  
+import org.w3c.dom.NodeList;  
+import org.w3c.dom.Node;  
+import org.w3c.dom.Element;  
+import java.io.File;
+import java.util.ArrayList;
+
+public class Libro extends Thread{
+    private String titulo;
+    private String biblioteca;
+    public ArrayList<String> libro = new ArrayList();
+
+    public Libro(String titulo, String biblioteca) {
+        this.titulo = titulo;
+        this.biblioteca = biblioteca;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getBiblioteca() {
+        return biblioteca;
+    }
+
+    public void setBiblioteca(String biblioteca) {
+        this.biblioteca = biblioteca;
+    }
+
+
+    @Override
+    public void run(){
+        String biblioteca = this.getBiblioteca();
+        if(biblioteca.equals("")){   
+            this.setBiblioteca("C");
+        }
+        try {
+            //Thread.sleep(8000);
+            File file = new File("src/DB/books.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();  
+            Document doc = db.parse(file);
+            doc.getDocumentElement().normalize();
+            //System.out.println("Root element: " + doc.getDocumentElement().getNodeName()); 
+            
+            NodeList nodeList = doc.getElementsByTagName("libro");
+            
+            for (int itr = 0; itr < nodeList.getLength(); itr++){
+                Node node = nodeList.item(itr);
+                //System.out.println("\nNode Name :" + node.getNodeName());  
+                if (node.getNodeType() == Node.ELEMENT_NODE){  
+                    Element eElement = (Element) node;
+                    if (eElement.getElementsByTagName("titulo").item(0).getTextContent().contentEquals(this.getTitulo())) {
+                        System.out.println("Libro solicitado por cliente de la biblioteca "+this.getBiblioteca()+" : \n");
+                        libro.add(eElement.getElementsByTagName("titulo").item(0).getTextContent());
+                        libro.add(eElement.getElementsByTagName("autor").item(0).getTextContent());
+                        libro.add(eElement.getElementsByTagName("editorial").item(0).getTextContent());
+                        libro.add(eElement.getElementsByTagName("fecha").item(0).getTextContent());
+                        System.out.println("Titulo: "+ eElement.getElementsByTagName("titulo").item(0).getTextContent());  
+                        System.out.println("Autor: "+ eElement.getElementsByTagName("autor").item(0).getTextContent());  
+                        System.out.println("Editorial: "+ eElement.getElementsByTagName("editorial").item(0).getTextContent());  
+                        System.out.println("Fecha: "+ eElement.getElementsByTagName("fecha").item(0).getTextContent()+"\n");
+                    }
+                }
+            }
+            
+        }
+        catch (Exception e){  
+            e.printStackTrace();
+        } 
+        
+    }
+
+    
+}
---
+++ SV RMI C/src/Main/ServerRMI.java
@@ -0,0 +1,111 @@
+package Main;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alexis
+ */
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.xml.parsers.DocumentBuilderFactory;  
+import javax.xml.parsers.DocumentBuilder;  
+import org.w3c.dom.Document;  
+import org.w3c.dom.NodeList;  
+import org.w3c.dom.Node;  
+import org.w3c.dom.Element;  
+import java.io.File;
+import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ServerRMI extends UnicastRemoteObject implements Middleware {
+    public ServerRMI() throws RemoteException {
+        super();
+    }
+    
+    @Override
+    public ArrayList encontrarVol(String valor,String biblioteca) throws RemoteException {
+        Libro libro = new Libro(valor,biblioteca);
+        try{
+            //Crear el hilo
+            
+            //Ejecutamos el hilo en un bloque de synchronized
+            synchronized(this){
+                libro.start();
+                libro.join();
+            }
+    
+            return libro.libro;
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return libro.libro;
+    }
+    
+    @Override
+    public ArrayList encontrarAutor(String valor, String biblioteca) throws RemoteException {
+        Autor autor = new Autor(valor,biblioteca);
+        try{
+            //Crear el hilo
+            //Ejecutamos el hilo en un bloque de synchronized
+            synchronized(this){
+                autor.start();
+                autor.join();
+            }
+    
+            return autor.libros;
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return autor.libros;
+    }
+    
+    @Override
+    synchronized public ArrayList getTitle(String value, String biblioteca) throws RemoteException {
+        ArrayList libro = new ArrayList();
+        libro = this.encontrarVol(value,biblioteca);
+        return libro; 
+
+    }
+    
+    @Override
+    synchronized public ArrayList getAuthor(String value, String biblioteca) throws RemoteException {
+        ArrayList<String> libro = new ArrayList();
+        libro = this.encontrarAutor(value,biblioteca);
+        return libro;
+    }   
+    
+    public static void main(String[] args) {
+        InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            
+            java.lang.System.setProperty("java.rmi.server.hostname", "127.0.0.1");
+            Registry registro = LocateRegistry.createRegistry(7779);
+            registro.rebind("RemoteRMIC", new ServerRMI()); //mantenemos el servidor en escucha
+            System.out.println("Servidor Corriendo en: " + ip);
+            System.out.println("Servidor Corriendo en: " + hostname);
+        } catch (RemoteException ex) {
+            System.out.println(ex.getMessage());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
